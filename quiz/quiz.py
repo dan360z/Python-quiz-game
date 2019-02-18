90d51d99d69c992dd442ccce73a6ff2e782ac782
Daniel Field <dannyf506@googlemail.com>
2019-02-18 19:09:37 +0000

    Added add a question option

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -2,3 +2,34 @@ def show_menu():
     print("1. Ask questions")
     print("2. Add a question")
     print("3. Exit game")
+    
+    option = input("Enter option: ")
+    return option 
+    
+def add_question():
+    print("")
+    question = input("Enter a question\n> ")
+    
+    print("")
+    print("Ok then, tell me the answer")
+    answer = input("{0}\n> ".format(question))
+    
+    file = open("questions.txt", "a")
+    file.write(question + "\n")
+    file.write(answer + "\n")
+    file.close()
+    
+def game_loop():
+    while True:
+        option = show_menu()
+        if option == "1":
+            print("You selected 'Ask question'")
+        elif option == "2":
+            add_question()
+        elif option == "3":
+            break
+        else:
+            print("Invalid option")
+        print("") 
+        
+game_loop()        
